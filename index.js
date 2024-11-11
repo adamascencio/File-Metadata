c816904a4be51b27234fee895f840b2fb28cad87
adamascencio <ascencioadam@gmail.com>
2024-11-11 09:37:43 -0800

    Feat: add /api/fileanalyse POST route handler for handling uploaded files

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+const upload = multer()
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.any(), (req, res) => {
+  const file = req.files[0];
+  const name = file.originalname;
+  const type = file.mimetype;
+  const size = file.size;
+  res.json({ name, type, size })
+});
 
 
 const port = process.env.PORT || 3000;
